2e614d7cc8638cf730187221b4b3eebd4832c41d
Fefe973 <90038730+Fefe973@users.noreply.github.com>
2023-01-06 20:13:52 +0100

    Update routes.ts

--- routes.ts
+++ routes.ts
@@ -24,7 +24,7 @@ router.get('/OBV/register',(ctx:any)=>{
 
 
 
-router.get('/OBV/connexion',verifConnectionConnection);
+//router.get('/OBV/connexion',verifConnectionConnection);
 
 router.get('/vanillastar.deno.dev',verifConnectionConnection);
 
